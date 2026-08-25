ca8d5c3c13d11c1a3aefde6e66fb6b8b5514501b
xuejiakang69 <xuejiakang69@gmail.com>
2026-08-25 14:00:21 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,6 +7,9 @@ const service = axios.create({
   timeout: 15000,
 })
 
+console.log('当前 API 地址:', import.meta.env.VITE_API_BASE_URL)
+console.log('Axios baseURL:', service.defaults.baseURL)
+
 // 请求拦截器
 service.interceptors.request.use(
   (config) => {
