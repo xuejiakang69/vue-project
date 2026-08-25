aeb240413415c49a38a673fc6f0ec08d6e137016
unknown <xuejiakang@shantaijk.cn>
2026-08-25 13:41:06 +0800

    修改api路径

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,7 @@ import router from '@/router'
 
 // 创建 axios 实例
 const service = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // 自动读取 .env 中的 API 前缀
+  baseURL: import.meta.env.VITE_API_BASE_URL, // 自动读取 .env 中的 API 前缀
   timeout: 15000,
 })
 
